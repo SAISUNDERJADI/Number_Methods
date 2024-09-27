2e01d41a95bb0f097a458017483d483a416f7a05
saisunderjadi <saisunderjadi@gmail.com>
2024-09-27 16:59:40 +0530

    Number Methods in JS

---
+++ Number Methods.js
@@ -0,0 +1,25 @@
+// Number Methods
+
+let num=3.1419
+console.log(num.toFixed(3))
+
+let str=10.5
+console.log(parseInt(str))
+console.log(parseFloat(str))
+
+
+let number=123.5467
+console.log(number.toPrecision(3))
+
+
+console.log(Number.isInteger(5))
+console.log(Number.isInteger(6.4))
+console.log(Number.isInteger(-4))
+console.log(Number.isInteger(-2.5))
+
+
+console.log(Number.isNaN(NaN))
+console.log(Number.isNaN(6))
+
+console.log(Number.isFinite(123.456))
+
